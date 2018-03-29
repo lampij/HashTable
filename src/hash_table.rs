7e5e08a45140db3ec5f88ebfcd47032b89fa96ef
bpinter <ben.david.pinter@gmail.com>
2018-03-29 14:10:35 -0400

    Added tests, created REALLY basic hash function, if you wanna call it that.

---
+++ src/hash_table.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+pub struct HashTable {
+	pub size: u8,
+	internal_collection: Vec<String>
+}
+
+impl HashTable {
+	pub fn add(&mut self, key: &String, value: &String) {
+		self.internal_collection.push(value.clone());
+		self.size += 1;
+	}
+
+	pub fn get(&self, key: &String) -> String {
+		String::from("")
+	}
+
+	pub fn new() -> HashTable {
+		HashTable {size: 0, internal_collection: vec!(String::from(""); 10000)}
+	}
+
+}
+
+pub fn hash_fun(mut key: &str) -> usize {
+	let mut table_location: u64 = 0;
+	let mut key_as_byte_array = key.as_bytes();
+	for byte in key_as_byte_array {
+		let tmp_tab_loc = (table_location + u64::from(*byte));
+		table_location = tmp_tab_loc.clone();
+	}
+
+	table_location as usize
+}
--- src/main.rs
+++ src/main.rs
@@ -27,3 +27,29 @@ fn get_from_hashtable(){
     let value = a.get(&key);
     assert_eq!(&value, &val);
 }
+
+#[test]
+fn collision_testing() {
+    let a = hash_table::hash_fun("test");
+    let b = hash_table::hash_fun("rest");
+    let c = hash_table::hash_fun("chortle");
+    let d = hash_table::hash_fun("orange");
+    let e = hash_table::hash_fun("poplar");
+
+    //Probably the ugliest test ever
+    assert_ne!(a, b);
+    assert_ne!(a, c);
+    assert_ne!(a, d);
+    assert_ne!(a, e);
+
+    assert_ne!(b, c);
+    assert_ne!(b, d);
+    assert_ne!(b, e);
+
+    assert_ne!(c, d);
+    assert_ne!(c, e);
+
+    assert_ne!(d, e);
+
+
+}
