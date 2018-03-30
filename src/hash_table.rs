b606de4503e20bc25387abf3f895ef3834656806
bpinter <ben.david.pinter@gmail.com>
2018-03-30 11:26:50 -0400

    Implemented add + get

--- src/hash_table.rs
+++ src/hash_table.rs
@@ -6,12 +6,13 @@ pub struct HashTable {
 
 impl HashTable {
 	pub fn add(&mut self, key: &String, value: &String) {
-		self.internal_collection.push(value.clone());
+		self.internal_collection[hash_fun(&key)] = value.clone();
+		//self.internal_collection.push(value.clone());
 		self.size += 1;
 	}
 
 	pub fn get(&self, key: &String) -> String {
-		String::from("")
+		self.internal_collection[hash_fun(&key)].clone()
 	}
 
 	pub fn new() -> HashTable {
@@ -24,7 +25,7 @@ pub fn hash_fun(mut key: &str) -> usize {
 	let mut table_location: u64 = 0;
 	let mut key_as_byte_array = key.as_bytes();
 	for byte in key_as_byte_array {
-		let tmp_tab_loc = (table_location + u64::from(*byte));
+		let tmp_tab_loc = table_location + u64::from(*byte);
 		table_location = tmp_tab_loc.clone();
 	}
 
